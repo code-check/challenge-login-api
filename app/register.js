54b84d56c04752efe617c57aa45d7d57810f321f
sukeshni <sukeshni.kantrod@gmail.com>
2015-11-18 16:12:33 +0900

    Implement Login api challenge in express

---
+++ app/register.js
@@ -0,0 +1,41 @@
+// You can use this file for Step1
+//BEGIN_CHALLENGE
+
+var index = require('./index.js');
+
+exports.handle = function (req, res, next) {
+  var data = req.body;
+
+  if (data.password.length < 6 || data.email == 'invalid') {
+    res.send({
+      'username': data.username,
+      'result': false,
+      'reason': 'Something went wrong'
+    });
+    return next();
+  }
+
+  if (index.findAccount(data.username)) {
+    res.send({
+      'username': data.username,
+      'result': false,
+      'reason': 'Account already exists'
+    });
+    return next();
+  }
+
+  index.pushAccount({
+    'username': data.username,
+    'password': data.password,
+    'email': data.email,
+    'deleted': false
+  });
+
+  res.send({
+    'username': data.username,
+    'result': true
+  });
+  return next();
+};
+
+//END_CHALLENGE
